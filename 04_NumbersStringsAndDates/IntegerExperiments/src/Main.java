6e0252ed31135d40064b26054ec8ecf369e440aa
unknown <diz.domination@gmail.com>
2020-09-01 15:26:50 +0300

    home work of lesson 4.1 added

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -5,11 +5,30 @@ public class Main
         Container container = new Container();
         container.count += 7843;
 
+        System.out.println(sumDigits(12345));
+        System.out.println(sumDigits(10));
+        System.out.println(sumDigits(5059191));
     }
 
-    public Integer sumDigits(Integer number)
+    public static Integer sumDigits(Integer number)
     {
         //@TODO: write code here
-        return 0;
+        Integer sum = 0;
+        String str = Integer.toString(number);
+        int[] arr = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            arr[i] = Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+
+
+        for (int i: arr)
+        {
+            sum += i;
+        }
+
+
+        return sum;
     }
 }
