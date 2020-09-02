179be7e8e6bcffa53ac61d8e35d9bb0ff35f01ec
unknown <diz.domination@gmail.com>
2020-09-02 14:41:02 +0300

    home work of lesson 4.2 added

---
+++ 04_NumbersStringsAndDates/MinMaxNumbers/src/MinMaxNumbers.java
@@ -0,0 +1,38 @@
+public class MinMaxNumbers
+{
+    public static void main(String[] args)
+    {
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("Тип byte.");
+        System.out.println("Минимальное отрицательное значение: " + Byte.MIN_VALUE);
+        System.out.println("Максимальное положительное значение: " + Byte.MAX_VALUE);
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("Тип short.");
+        System.out.println("Минимальное отрицательное значение: " + Short.MIN_VALUE);
+        System.out.println("Максимальное положительное значение: " + Short.MAX_VALUE);
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("Тип int.");
+        System.out.println("Минимальное отрицательное значение: " + Integer.MIN_VALUE);
+        System.out.println("Максимальное положительное значение: " + Integer.MAX_VALUE);
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("Тип long.");
+        System.out.println("Минимальное отрицательное значение: " + Long.MIN_VALUE);
+        System.out.println("Максимальное положительное значение: " + Long.MAX_VALUE);
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("Тип float.");
+        System.out.println("Минимальное отрицательное значение: " + (-Float.MAX_VALUE));
+        System.out.println("Максимальное положительное значение: " + Float.MAX_VALUE);
+        System.out.println("------------------------------------------------------");
+
+        System.out.println("Тип double.");
+        System.out.println("Минимальное отрицательное значение: " + (-Double.MAX_VALUE));
+        System.out.println("Максимальное положительное значение: " + Double.MAX_VALUE);
+        System.out.println("------------------------------------------------------");
+
+    }
+}
