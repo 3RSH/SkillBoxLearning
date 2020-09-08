ddd012adf3eace4ee27379d266ddd1fd330bdfc0
unknown <diz.domination@gmail.com>
2020-09-08 20:01:46 +0300

    solution of the task №2 of lesson 4.4 added

--- 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
+++ 04_NumbersStringsAndDates/StringExperiments/src/Loader.java
@@ -1,3 +1,4 @@
+import static java.lang.Integer.parseInt;
 
 public class Loader
 {
@@ -6,5 +7,43 @@ public class Loader
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
 
         System.out.println(text);
+        System.out.println("Общая сумма чисел в строке: " + getSum(text));
+    }
+
+    //метод извлекает ВСЕ числа из строки, не зависимо от содержимого
+    public static int getSum(String str)
+    {
+        int sum = 0;
+        int beginIndex = -1;
+        int endIndex = -1;
+        String sumString;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            int code = str.charAt(i);
+
+            if (code > 47 && code < 58) {
+                if (i == 0 || beginIndex == -1) {
+                    beginIndex = i;
+                }
+                endIndex = i + 1;
+
+            } else {
+                if (beginIndex != -1){
+                    sumString = str.substring(beginIndex, endIndex);
+                    sum += parseInt(sumString);
+                }
+
+                beginIndex = -1;
+                endIndex = -1;
+            }
+
+            //корректное чтение чисел в конце строки
+            if (i == (str.length() - 1) && beginIndex != -1) {
+                sumString = str.substring(beginIndex);
+                sum += parseInt(sumString);
+            }
+        }
+        return sum;
     }
 }
