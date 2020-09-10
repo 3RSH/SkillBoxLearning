e3aab2814daf6421d4f03867de2ac8c2432a92bc
unknown <diz.domination@gmail.com>
2020-09-10 06:29:08 +0300

    solution of the task №3 of lesson 4.4 corrected

--- 04_NumbersStringsAndDates/FullName/src/Main.java
+++ 04_NumbersStringsAndDates/FullName/src/Main.java
@@ -6,13 +6,59 @@ public class Main
     {
         Scanner scanner = new Scanner(System.in);
         String[] name = {"", "", ""};
-        String fullName = scanner.nextLine();
-        String[] SplitName = fullName.split(" ", 3);
 
-        for (int i = 0; i < SplitName.length; i++) {
-            name[i] = SplitName[i];
+        while (true) {
+            System.out.print("Введите Фамилию Имя Отчество: ");
+            String fullName = scanner.nextLine();
+            int spaceCount = 0;
+
+            for (int i = 0; i < fullName.length(); i++) {
+                if (fullName.charAt(i) == ' ') {
+                    spaceCount++;
+                }
+            }
+
+            //проверка количества пробелов
+            if (spaceCount == 2) {
+                int enterSpace = fullName.indexOf(' ');
+                int exitSpace = fullName.lastIndexOf(' ');
+                //проверка_1 - последовательность пробелов
+                boolean check1 = (exitSpace - enterSpace) > 1;
+                //проверка_2 - пробел в начале строки
+                boolean check2 = enterSpace > 0;
+                //проверка_3 - пробел в конце строки
+                boolean check3 = exitSpace < (fullName.length()-1);
+
+                if (check1 && check2 && check3) {
+                    int code1 = fullName.charAt(0);
+                    int code2 = fullName.charAt(enterSpace + 1);
+                    int code3 = fullName.charAt(enterSpace + 1);
+                    //проверка 1-го слова на наличие первой заглавной буквы
+                    boolean checkCode1 = ((code1 > 64) && (code1 < 91)) || ((code1 > 1039) && (code1 < 1072));
+                    //проверка 2-го слова на наличие первой заглавной буквы
+                    boolean checkCode2 = ((code2 > 64) && (code2 < 91)) || ((code2 > 1039) && (code2 < 1072));
+                    //проверка 3-го слова на наличие первой заглавной буквы
+                    boolean checkCode3 = ((code3 > 64) && (code3 < 91)) || ((code3 > 1039) && (code3 < 1072));
+
+                    if (checkCode1 && checkCode2 && checkCode3) {
+                        name[0] = fullName.substring(0, enterSpace);
+                        name[1] = fullName.substring(enterSpace + 1, exitSpace);
+                        name[2] = fullName.substring(exitSpace + 1);
+                        break;
+                    }
+                    System.out.println(errorMessage());
+                    continue;
+                }
+                System.out.println(errorMessage());
+                continue;
+            }
+            System.out.println(errorMessage());
         }
-
         System.out.println("Фамилия: " + name[0] + "\nИмя: " + name[1] + "\nОтчество: " + name[2]);
+    }
+
+    public static String errorMessage()
+    {
+        return "Данные введены некорректно!";
     }
 }
