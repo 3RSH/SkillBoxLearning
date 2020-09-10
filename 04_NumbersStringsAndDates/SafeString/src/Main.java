e0eee8d22c20973811bd3a17933160f524793089
unknown <diz.domination@gmail.com>
2020-09-10 06:51:26 +0300

    solution of lesson 4.4** added

--- 04_NumbersStringsAndDates/SafeString/src/Main.java
+++ 04_NumbersStringsAndDates/SafeString/src/Main.java
@@ -2,17 +2,22 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***");
+        String safe = searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912" +
+                " пользователя <Петрова> Алексея", "***");
         System.out.println(safe);
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder)
     {
-        int beginIndex = text.indexOf('<');
-        int endIndex = text.lastIndexOf('>') + 1;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '<' && text.contains(">")) {
+                int beginIndex = text.indexOf('<');
+                int endIndex = text.indexOf('>') + 1;
 
-        if (beginIndex < endIndex) {
-            text = text.replaceAll(text.substring(beginIndex, endIndex), placeholder);
+                if (beginIndex < endIndex) {
+                    text = text.replaceAll(text.substring(beginIndex, endIndex), placeholder);
+                }
+            }
         }
         return text;
     }
