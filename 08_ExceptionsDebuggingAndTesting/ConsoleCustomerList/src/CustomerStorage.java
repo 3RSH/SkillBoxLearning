41965a03191d82ccb84a5db5422f196341eac490
unknown <diz.domination@gmail.com>
2020-10-27 20:20:04 +0300

    home work 8.1 added

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -2,6 +2,13 @@ import java.util.HashMap;
 
 public class CustomerStorage
 {
+    private static final String ADDING_FORMAT_MESSAGE = "Correct format example:\n"
+        + "add Василий Петров vasily.petrov@gmail.com +79215637722";
+    private static final String NAME_FORMAT = "[A-ZА-Я].+";
+    private static final String MAIL_FORMAT = "[a-z][-._a-z0-9]{0,28}[a-z0-9]@"
+        + "[a-z][-a-z0-9]{0,29}[a-z0-9]\\.[a-z]{2,3}";
+    private static final String PHONE_FORMAT = "\\+79\\d{9}";
+
     private HashMap<String, Customer> storage;
 
     public CustomerStorage()
@@ -12,6 +19,31 @@ public class CustomerStorage
     public void addCustomer(String data)
     {
         String[] components = data.split("\\s+");
+
+        //проверка количества введённых данных
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong adding format! "
+                + ADDING_FORMAT_MESSAGE);
+        }
+
+        //проверка правильности ввода Имени-Фамилии
+        if (!components[0].matches(NAME_FORMAT) || !components[1].matches(NAME_FORMAT)) {
+            throw new IllegalArgumentException("Wrong adding name format! "
+                + ADDING_FORMAT_MESSAGE);
+        }
+
+        //проверка правильности ввода почты
+        if (!components[2].matches(MAIL_FORMAT)) {
+            throw new IllegalArgumentException("Wrong adding e-mail format! "
+                + ADDING_FORMAT_MESSAGE);
+        }
+
+        //проверка правильности ввода номера
+        if (!components[3].matches(PHONE_FORMAT)) {
+            throw new IllegalArgumentException("Wrong adding number format! "
+                + ADDING_FORMAT_MESSAGE);
+        }
+
         String name = components[0] + " " + components[1];
         storage.put(name, new Customer(name, components[3], components[2]));
     }
--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/Main.java
@@ -18,24 +18,38 @@ public class Main
         {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
-            if(tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("list")) {
-                executor.listCustomers();
-            }
-            else if(tokens[0].equals("remove"))
-            {
-                executor.removeCustomer(tokens[1]);
-            }
-            else if(tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            }
-            else if(tokens[0].equals("help")) {
-                System.out.println(helpText);
-            }
-            else {
-                System.out.println(commandError);
+
+            try {
+                if(tokens[0].equals("add")) {
+                    if (tokens.length != 2) {
+                        throw new IllegalArgumentException(commandError);
+                    }
+
+                    executor.addCustomer(tokens[1]);
+                }
+                else if(tokens[0].equals("list")) {
+                    executor.listCustomers();
+                }
+                else if(tokens[0].equals("remove"))
+                {
+                    if (tokens.length != 2) {
+                        throw new IllegalArgumentException(commandError);
+                    }
+
+                    executor.removeCustomer(tokens[1]);
+                }
+                else if(tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                }
+                else if(tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                }
+                else {
+                    System.out.println(commandError);
+                }
+
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
             }
         }
     }
