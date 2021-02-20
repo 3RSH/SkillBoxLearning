8f1c33330420a93a06fa8beba7cf650ed5320c6a
unknown <diz.domination@gmail.com>
2021-02-20 22:55:19 +0300

    home work 14.1 corrected again

--- 14_Performance/CarNumberGenerator/src/Loader.java
+++ 14_Performance/CarNumberGenerator/src/Loader.java
@@ -1,53 +1,18 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Loader {
 
   public static void main(String[] args) throws Exception {
+
     long start = System.currentTimeMillis();
-
-    List<String> numbers = new ArrayList<>();
-
-    char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-
-    for (int regionCode = 1; regionCode < 100; regionCode++) {
-
-      String regionStr = padNumber(regionCode, 2);
-      
-      for (int number = 1; number < 1000; number++) {
-
-        String numberStr = padNumber(number, 3);
-        StringBuilder builder = new StringBuilder();
-
-        for (char firstLetter : letters) {
-
-          for (char secondLetter : letters) {
-
-            for (char thirdLetter : letters) {
-
-              builder.append(firstLetter)
-                  .append(numberStr)
-                  .append(secondLetter)
-                  .append(thirdLetter)
-                  .append(regionStr)
-                  .append("\n");
-            }
-          }
-        }
-
-        numbers.add(builder.toString());
-      }
-    }
 
     //Вывод номеров в несколько файлов из нескольких потоков(4-е потока, для примера)
 
     List<NumberGenerator> threads = new ArrayList<>();
 
     for (int i = 0; i < 4; i++) {
-      threads.add(new NumberGenerator(i, numbers));
+      threads.add(new NumberGenerator(i));
     }
 
     for (NumberGenerator nGen : threads) {
@@ -56,15 +21,5 @@ public class Loader {
     }
 
     System.out.println((System.currentTimeMillis() - start) + " ms");
-
-  }
-
-  private static String padNumber(int number, int numberLength) {
-    String numberStr = String.valueOf(number);
-    int padSize = numberLength - numberStr.length();
-
-    return (padSize < 1)
-        ? numberStr
-        : ("0".repeat(padSize) + numberStr);
   }
 }
--- 14_Performance/CarNumberGenerator/src/NumberGenerator.java
+++ 14_Performance/CarNumberGenerator/src/NumberGenerator.java
@@ -1,24 +1,60 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.List;
 
 public class NumberGenerator extends Thread {
 
-  private final List<String> numbers;
   private String path = "14_Performance/CarNumberGenerator/res/numbers#num.txt";
 
-  public NumberGenerator(int index, List<String> numbers) {
+  public NumberGenerator(int index) {
     this.path = path.replaceFirst("#num", String.valueOf(index));
-    this.numbers = numbers;
+  }
+
+  private static String padNumber(int number, int numberLength) {
+    String numberStr = String.valueOf(number);
+    int padSize = numberLength - numberStr.length();
+
+    return (padSize < 1)
+        ? numberStr
+        : ("0".repeat(padSize) + numberStr);
   }
 
   @Override
   public void run() {
 
-    try (PrintWriter writer = new PrintWriter(path)) {
-      numbers.forEach(writer::write);
-      writer.flush();
+    char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
 
+    try (PrintWriter writer = new PrintWriter(path)) {
+
+      for (int regionCode = 1; regionCode < 100; regionCode++) {
+
+        String regionStr = padNumber(regionCode, 2);
+
+        for (int number = 1; number < 1000; number++) {
+
+          String numberStr = padNumber(number, 3);
+          StringBuilder builder = new StringBuilder();
+
+          for (char firstLetter : letters) {
+
+            for (char secondLetter : letters) {
+
+              for (char thirdLetter : letters) {
+
+                builder.append(firstLetter)
+                    .append(numberStr)
+                    .append(secondLetter)
+                    .append(thirdLetter)
+                    .append(regionStr)
+                    .append("\n");
+              }
+            }
+          }
+
+          writer.write(builder.toString());
+        }
+      }
+
+      writer.flush();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
